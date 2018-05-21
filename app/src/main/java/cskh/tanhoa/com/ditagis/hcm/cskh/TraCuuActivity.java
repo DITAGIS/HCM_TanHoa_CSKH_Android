8477f2f56d22a120c210f7e24eb777c5465f8f46
thanle95 <lengocthan95@gmail.com>
2018-05-21 22:51:43 +0700

    Thiet ke giao dien so bo

---
+++ app/src/main/java/cskh/tanhoa/com/ditagis/hcm/cskh/TraCuuActivity.java
@@ -0,0 +1,13 @@
+package cskh.tanhoa.com.ditagis.hcm.cskh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TraCuuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tra_cuu);
+    }
+}
